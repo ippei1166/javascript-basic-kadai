01e143cbc0d09b45c54be2d717c9c10cc1e73e5c
ippei1166 <152014027+ippei1166@users.noreply.github.com>
2024-01-10 13:07:22 +0900

    kadai20追加

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const eventBtn = document.getElementById('btn');
+const btnText = document.getElementById('text');
+
+eventBtn.addEventListener('click',() =>{
+btnText.textContent = 'ボタンを押しました'
+});
